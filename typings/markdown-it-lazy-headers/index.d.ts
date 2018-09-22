e8ca4abc010a9cd0394f530cdbee663860225a4b
Max Knor <mknor@microsoft.com>
2018-09-22 11:34:55 +0200

    Added custom D.TS declaration for module markdown-it-lazy-headers so the linter is satisfied.

---
+++ typings/markdown-it-lazy-headers/index.d.ts
@@ -0,0 +1,12 @@
+// Type definitions for MarkdownItLazyHeaders (markdown-it-lazy-headers) 0.13
+// Project: https://github.com/Galadirith/markdown-it-lazy-headers
+// Definitions by: Knom <https://github.com/knom>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.2
+declare module "markdown-it-lazy-headers" {
+    import { MarkdownIt, Core, Token } from 'markdown-it';
+
+    function lazyheaders(md: MarkdownIt): void;
+
+    export = lazyheaders;
+}
