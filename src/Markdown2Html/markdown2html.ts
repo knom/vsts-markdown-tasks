ee0d88ad0b1ead76e0e1d3b84d4f281fece4224e
knom <m.knor@gmx.net>
2018-09-22 11:35:27 +0200

    Fixed errors/warnings from TSLint

--- src/Markdown2Html/markdown2html.ts
+++ src/Markdown2Html/markdown2html.ts
@@ -1,63 +1,63 @@
-// Upload just build task to VSTS...
+/// <reference path="../../typings/markdown-it-lazy-headers/index.d.ts" />
+
+// upload just build task to VSTS...
 // tfx build tasks upload --task-path ./
 // tfx build tasks delete --task-id 7346cf50-6858-11e6-b59a-6fe0ce03f800
 
-// Upload extension VSTS...
+// upload extension VSTS...
 // tfx extension create --manifest-globs .\vss-extension.json
-// Upload to https://marketplace.visualstudio.com/manage/
+// upload to https://marketplace.visualstudio.com/manage/
 
 // toolrunner help
 // https://github.com/Microsoft/vsts-task-lib/blob/be60205671545ebef47d3a40569519da9b4d34b0/node/docs/vsts-task-lib.md
 
 
 // tsc -w
-// $env:INPUT_ewsConnectedServiceName = 'EP1'
-// $env:ENDPOINT_URL_EP1 = 'https://mail.office365.com/'
+// $env:INPUT_ewsConnectedServiceName = "EP1"
+// $env:ENDPOINT_URL_EP1 = "https://mail.office365.com/"
 // $env:ENDPOINT_AUTH_EP1 = '{ "parameters": { "username": "Some user", "password": "Some password" }, "scheme": "Some scheme" }'
 // $env:ENDPOINT_DATA_EP1 = '{ "Key1": "Value1", "Key2", "Value2" }'
 // $env:INPUT_appManifestXmlPath="C:\...\a.xml"
 
-import tl = require('vsts-task-lib/task');
-import trm = require('vsts-task-lib/toolrunner');
-import q = require('q');
-import fs = require('fs');
-import mdit = require('markdown-it');
-import lazyHeaders = require('markdown-it-lazy-headers');
-import dust = require('dustjs-linkedin');
-import util = require('util');
-import mditAnchor = require('markdown-it-anchor');
+import tl = require("vsts-task-lib/task");
+import trm = require("vsts-task-lib/toolrunner");
+import q = require("q");
+import fs = require("fs");
+import mdit = require("markdown-it");
+import lazyHeaders = require("markdown-it-lazy-headers");
+import dust = require("dustjs-linkedin");
+import util = require("util");
+import mditAnchor = require("markdown-it-anchor");
 
-function transformTemplate(templatePath: string, templateObject: any) {
-	let deferred = q.defer();
+function transformTemplate(templatePath: string, templateObject: any): any {
+	let deferred: q.Deferred<any> = q.defer();
 
 	if (templatePath) {
-		fs.readFile(templatePath, 'utf8', function (err: any, data: string) {
+		fs.readFile(templatePath, "utf8", function (err: any, data: string): void {
 			if (err) {
 				throw err;
 			}
 
 			tl.debug(util.format("Applying HTML template using parameters %j...", JSON.stringify(templateObject)));
 
-			dust.renderSource(data, templateObject, function (err: any, out: string) {
+			dust.renderSource(data, templateObject, function (err: any, out: string): void {
 				tl.debug("Applying HTML template succeeded!");
 
 				if (err) {
 					throw err;
-				}
-				else {
+				} else {
 					deferred.resolve(out);
 				}
 			});
 		});
-	}
-	else {
+	} else {
 		deferred.resolve(templateObject.body);
 	}
 
 	return deferred.promise;
 }
 
-function throwIfDirectory(parameter: string, path: string) {
+function throwIfDirectory(parameter: string, path: string): void {
 	if (!path) {
 		return;
 	}
@@ -67,66 +67,65 @@ function throwIfDirectory(parameter: string, path: string) {
 	}
 
 	if (fs.lstatSync(path).isDirectory()) {
-		let message = util.format("Parameter '%s=%s' should be a file but is a directory!", parameter, path);
+		let message: string = util.format("Parameter '%s=%s' should be a file but is a directory!", parameter, path);
 
 		throw { message: message };
 	}
 }
 
-function run() {
+function run(): void {
 	try {
-		let sourcesPath = tl.getVariable("System.DefaultWorkingDirectory");
+		let sourcesPath: string = tl.getVariable("System.DefaultWorkingDirectory");
 
-		let markdownPath = tl.getPathInput('markdownPath', true, true);
-		let htmlPath = tl.getPathInput('htmlPath', true);
+		let markdownPath: string = tl.getPathInput("markdownPath", true, true);
+		let htmlPath: string = tl.getPathInput("htmlPath", true);
 
-		let templatePath = tl.getPathInput('templatePath', false, true);
+		let templatePath: string = tl.getPathInput("templatePath", false, true);
 
-		if (templatePath === sourcesPath)
+		if (templatePath === sourcesPath) {
 			templatePath = null;
+		}
 
-		let parameters = tl.getInput('parameters', false);
+		let parameters: string = tl.getInput("parameters", false);
 
 		throwIfDirectory("markdownPath", markdownPath);
 		throwIfDirectory("htmlPath", htmlPath);
 		throwIfDirectory("templatePath", templatePath);
 
 		tl.debug("Reading markdown file " + markdownPath + " (UTF-8)...");
-		fs.readFile(markdownPath, 'utf8', function (err, data) {
+		fs.readFile(markdownPath, "utf8", function (err: any, data: any): void {
 			if (err) {
 				throw err;
 			}
 
 			tl.debug("Reading file " + markdownPath + " succeeded!");
 
-			var md = mdit();
+			var md: mdit.MarkdownIt = mdit();
 			md.use(lazyHeaders);
 			md.use(mditAnchor, {
 				level: 1,
 				permalink: false
-			})
+			});
 
 			tl.debug("Rendering markdown to html...");
-			var result = md.render(data);
+			var result: any = md.render(data);
 			tl.debug("Rendering markdown to html succeeded!");
 
-			let parametersObject = null;
+			let parametersObject: any = null;
 
 			if (!parameters) {
 				parametersObject = { body: result };
-			}
-			else {
+			} else {
 				try {
 					parametersObject = JSON.parse(parameters);
-				}
-				catch (ex) {
+				} catch (ex) {
 					tl.setResult(tl.TaskResult.Failed, util.format("Parameter \"Paramaters\" contains illegal JSON \"%s\"", parameters));
 					return;
 				}
 				parametersObject.body = result;
 			}
 
-			transformTemplate(templatePath, parametersObject).then(function (tresult: string) {
+			transformTemplate(templatePath, parametersObject).then(function (tresult: string): any {
 				tl.debug("Writing HTML file " + htmlPath + "...");
 				fs.writeFileSync(htmlPath, tresult);
 				tl.debug("Writing HTML file " + htmlPath + " succeeded!");
@@ -134,8 +133,7 @@ function run() {
 				tl.setResult(tl.TaskResult.Succeeded, "Successfully transformed markdown file " + markdownPath + " to HTML file " + htmlPath);
 			});
 		});
-	}
-	catch (err) {
+	} catch (err) {
 		// handle failures in one place
 		tl.setResult(tl.TaskResult.Failed, err.message);
 	}
