bf48b5298d64965d1056e87b256b6344cc7ad9ec
knom <m.knor@gmx.net>
2018-10-02 15:03:29 +0200

    Updated for TSLint

--- test/Parameters_Mock.ts
+++ test/Parameters_Mock.ts
@@ -1,20 +1,20 @@
+import path = require("path");
 import ma = require("vsts-task-lib/mock-answer");
 import tmrm = require("vsts-task-lib/mock-run");
-import path = require("path");
 
-let taskPath: string = path.join(__dirname, "..", "src", "Markdown2Html", "markdown2html.js");
-let markdownPath: string = path.join(__dirname, "sample-md-files", "Parameters.md");
-let htmlPath: string = path.join(__dirname, "sample-md-files", "Output.html");
-let templatePath: string = path.join(__dirname, "sample-md-files", "Parameters.html");
+const taskPath: string = path.join(__dirname, "..", "src", "Markdown2Html", "markdown2html.js");
+const markdownPath: string = path.join(__dirname, "sample-md-files", "Parameters.md");
+const htmlPath: string = path.join(__dirname, "sample-md-files", "Output.html");
+const templatePath: string = path.join(__dirname, "sample-md-files", "Parameters.html");
 
-let taskMock: tmrm.TaskMockRunner = new tmrm.TaskMockRunner(taskPath);
+const taskMock: tmrm.TaskMockRunner = new tmrm.TaskMockRunner(taskPath);
 taskMock.setInput("markdownPath", markdownPath);
 taskMock.setInput("htmlPath", htmlPath);
 taskMock.setInput("templatePath", templatePath);
-taskMock.setInput("parameters", JSON.stringify({ "header": { "title": "My Title" } }));
+taskMock.setInput("parameters", JSON.stringify({ header: { title: "My Title" } }));
 
-let answers: ma.TaskLibAnswers = <ma.TaskLibAnswers>{
-    "checkPath": {}
+const answers: ma.TaskLibAnswers = <ma.TaskLibAnswers>{
+    checkPath: {},
 };
 answers.checkPath[markdownPath] = true;
 answers.checkPath[templatePath] = true;
--- test/Template_Mock.ts
+++ test/Template_Mock.ts
@@ -1,19 +1,19 @@
+import path = require("path");
 import ma = require("vsts-task-lib/mock-answer");
 import tmrm = require("vsts-task-lib/mock-run");
-import path = require("path");
 
-let taskPath: string = path.join(__dirname, "..", "src", "Markdown2Html", "markdown2html.js");
-let markdownPath: string = path.join(__dirname, "sample-md-files", "Template.md");
-let htmlPath: string = path.join(__dirname, "sample-md-files", "Output.html");
-let templatePath: string = path.join(__dirname, "sample-md-files", "Template.html");
+const taskPath: string = path.join(__dirname, "..", "src", "Markdown2Html", "markdown2html.js");
+const markdownPath: string = path.join(__dirname, "sample-md-files", "Template.md");
+const htmlPath: string = path.join(__dirname, "sample-md-files", "Output.html");
+const templatePath: string = path.join(__dirname, "sample-md-files", "Template.html");
 
-let taskMock: tmrm.TaskMockRunner = new tmrm.TaskMockRunner(taskPath);
+const taskMock: tmrm.TaskMockRunner = new tmrm.TaskMockRunner(taskPath);
 taskMock.setInput("markdownPath", markdownPath);
 taskMock.setInput("htmlPath", htmlPath);
 taskMock.setInput("templatePath", templatePath);
 
-let answers: ma.TaskLibAnswers = <ma.TaskLibAnswers>{
-    "checkPath": {}
+const answers: ma.TaskLibAnswers = <ma.TaskLibAnswers>{
+    checkPath: {}, 
 };
 answers.checkPath[markdownPath] = true;
 answers.checkPath[templatePath] = true;
